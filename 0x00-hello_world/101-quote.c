f5d86e0695de8c7656ef4b78c264c4706fc81eb5
Enoch Justice Arthur <arthurenochjustice@gmail.com>
2022-10-13 21:56:01 -0700

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main -print quote
+ *
+ *Description: prints Dora's quote
+ *
+ *Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	write(2, "and that peice of art is useful\' - Dora Korpar, 2015-1019\n", 59);
+	return (1)
+}
