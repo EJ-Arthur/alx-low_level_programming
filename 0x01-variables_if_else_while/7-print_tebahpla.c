7d5c69e9fe79bab6975b5b9e1d242e8e2d387e8a
Enoch Justice Arthur <arthurenochjustice@gmail.com>
2022-10-14 21:48:58 -0700

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio>
+
+/**
+ *main - prints the lowercase alphabet inreverse followed by a new line
+ *
+ *Return: Always 0
+ */
+
+int main(void)
+{
+	char letter = 'z';
+
+	while (letter >= 'a')
+	{
+		putchar(letter);
+		letter--;
+	}
+
+	putchar('\n');
+}
