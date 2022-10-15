d99caca2bfe6f145ef920225252b61a8813626b9
Enoch Justice Arthur <arthurenochjustice@gmail.com>
2022-10-14 22:28:17 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - print all possible combinations of single-digit numbers
+ *
+ *Return - always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		putchar((i % 10) + '0');
+		putchar(',');
+		putchar(' ');
+	}
+
+	putchar('\n');
+	return (0);
+}
