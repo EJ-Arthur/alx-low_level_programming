79bb51e22514a36d5c8120d776e16d08319875a3
Enoch Justice Arthur <arthurenochjustice@gmail.com>
2022-10-14 22:02:36 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Print all the numbers of base 16
+ *        in lowercase
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int num = 0;
+	char alpha = a;
+
+	while (num < 10)
+	{
+		putchar((num % 10) + '0');
+		num++;
+	}
+
+	while (alpha <= 'f')
+	{
+		putchar(alpha);
+		alpha++;
+	}
+
+	putchar('\n');
+}
