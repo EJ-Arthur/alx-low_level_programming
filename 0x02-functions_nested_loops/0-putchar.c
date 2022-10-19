b2a38428e1412cf9ac6ce7c0da478ffc25dc2034
Enoch Justice Arthur <arthurenochjustice@gmail.com>
2022-10-18 18:33:06 -0700

    Task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ * Description - print _putchar followed by a new line
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
