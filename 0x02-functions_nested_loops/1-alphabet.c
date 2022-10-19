1a8f6e11e09e03f864756155e9a6f7133681d0f3
Enoch Justice Arthur <arthurenochjustice@gmail.com>
2022-10-19 15:54:58 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	_putchar('\n');
+
+	return (0);
+}
+
+int print_alphabet(void)
+{
+	char lett = 'a';
+
+	while (lett <= 'z')
+	{
+		_putchar(lett);
+		lett++;
+	}
+
+	return (0);
+}
