0adc2bd63e82560a6ccfd874378014424ae9f6ef
Enoch Justice Arthur <arthurenochjustice@gmail.com>
2022-10-19 15:17:44 -0700

    fix task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-	char text[9] = "_putchar";
+	char text[10] = "_putchar";
 	int i;
 
 	for (i = 0; i < 9; i++)
 	{
 		_putchar(text[i]);
 	}
-	/*_putchar('\n');*/
+	_putchar('\n');
 
 	return (0);
 }
