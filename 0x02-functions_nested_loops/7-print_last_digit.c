61b0cec0e32cfca2a37d8e571697e249c4b8c128
Enoch Justice Arthur <arthurenochjustice@gmail.com>
2022-10-19 20:59:00 -0700

    fix task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,5 +8,14 @@
 
 int print_last_digit(int c)
 {
-	return (c % 10);
+	int last_digit = c % 10;
+
+	if (last_digit < 0)
+	{
+		last_digit *= -1;
+	}
+
+	_putchar(last_digit + '0');
+
+	return (last_digit);
 }
