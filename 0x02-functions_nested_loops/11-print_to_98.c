41b05039d3168863313da4174f2cc7559603ac3f
Enoch Justice Arthur <arthurenochjustice@gmail.com>
2022-10-19 22:26:13 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -9,9 +10,9 @@
 
 void print_to_98(int n)
 {
-	if (n < 98)
+	if (n <= 98)
 	{
-		while (n <= 98)
+		while (n < 98)
 		{
 			_putchar(n);
 			_putchar(',');
