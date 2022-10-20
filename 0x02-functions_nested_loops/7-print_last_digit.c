a82deee62cd952897cbf9d84ca057b8e006015a5
Enoch Justice Arthur <arthurenochjustice@gmail.com>
2022-10-19 20:42:35 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * main -check code
+ * print_last_digit - prints the last difit of a number
+ * Return: Always 0
+ * @c: number to be checked
+ */
+
+int main(int c)
+{
+	return (c[-1]);
+}
