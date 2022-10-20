1f1958b78d102f4f470f5e880527d4636b0a78b9
Enoch Justice Arthur <arthurenochjustice@gmail.com>
2022-10-19 17:09:16 -0700

    task k 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabets in lowercase
+ */
+
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char letter = 'a';
+
+	while (i < 10)
+	{
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+			letter++;
+		}
+
+		_putchar('\n');
+	}
+}
