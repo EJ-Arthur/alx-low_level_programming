20b78756edab25238676ddcc586677a23a2127b7
Enoch Justice Arthur <arthurenochjustice@gmail.com>
2022-10-19 17:30:05 -0700

    fix task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,7 +9,7 @@ void print_alphabet_x10(void)
 	int i = 0;
 	char letter = 'a';
 
-	while (i < 10)
+	while (i++ < 10)
 	{
 		while (letter <= 'z')
 		{
@@ -18,6 +18,6 @@ void print_alphabet_x10(void)
 		}
 
 		_putchar('\n');
-		i++;
+
 	}
 }
