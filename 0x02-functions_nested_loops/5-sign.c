e366666957b07a1ea838d9cd229a5703bcc52906
Enoch Justice Arthur <arthurenochjustice@gmail.com>
2022-10-19 19:23:47 -0700

    fix task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,10 +13,10 @@ int print_sign(int n)
 		return (0);
 	} else if (n > 0)
 	{
-		_putchar('1');
+		_putchar('+');
 		return (1);
 	} else if (n < 0)
 	{
-		_putchar('-1');
+		_putchar('1');
 		return (-1);
 	}
