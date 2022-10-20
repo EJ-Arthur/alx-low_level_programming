b4a0f38630ebbbf28a55a828cb6aeed448c4f6c6
Enoch Justice Arthur <arthurenochjustice@gmail.com>
2022-10-19 21:48:35 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - Adds two integers
+ * @a: first number
+ * @b: second number
+ * Return: the sum of a and b
+ */
+
+int add(int a, int b)
+{
+	sum = a + b;
+	return (sum);
+}
