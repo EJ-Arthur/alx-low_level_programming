a7674cbe4de5c98a09e03139a73ed1c9258c2fc6
Enoch Justice Arthur <arthurenochjustice@gmail.com>
2022-10-19 23:03:21 -0700

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,21 +14,17 @@ void print_to_98(int n)
 	{
 		while (n < 98)
 		{
-			_putchar(n);
-			_putchar(',');
-			_putchar(' ');
-			n++;
+			printf("%d\n", n++);
 		}
+		printf("%d\n", n);
 	}
 	else
 	{
 		while (n > 98)
 		{
-			_putchar(n);
-			_putchar(',');
-			_putchar(' ');
-			n--;
+			printf("%d\n", n--);
 		}
+		printf("%d\n", n);
 	}
 
 	_putchar('\n');
