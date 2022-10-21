ec11e1a4304e567da83e88179039b2b7ef3d0e7f
Enoch Justice Arthur <arthurenochjustice@gmail.com>
2022-10-20 19:12:21 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; 1 <= 100; i++)
+	{
+		if (((i % 3) == 0) && ((i % 5) == 0))
+			printf("FizzBuzz");
+		else if ((i % 3) == 0)
+			printf("Fizz");
+		else if ((i % 5) == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+	}
+	_putchar(i);
+}
