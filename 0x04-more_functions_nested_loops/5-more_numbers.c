5b39c322ac2baa5ae2c37a388b2ed972012485c2
Enoch Justice Arthur <arthurenochjustice@gmail.com>
2022-10-20 17:27:26 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers
+ * from 0 to 14
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	char i = '0', ii;
+
+	while (i++ < '10')
+	{
+		for (ii = '0'; ii <= '14'; i++)
+		{
+			_putchar(ii);
+		}
+
+		_putchar('\n');
+	}
+}
