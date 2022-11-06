ca01f231d867acf32359a6441e1591cdabeb2af0
Enoch Justice Arthur <arthurenochjustice@gmail.com>
2022-11-06 06:25:57 -0800

    push task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string followed by
+ * a new line to stdout
+ * Return - void
+ * @str: string to print
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i]; i++)
+		_putchar(stri[i]);
+	_putchar('\n');
+}
