54e2ba2ae13626d39f8c1a8322f9f8dd03924aa4
Enoch Justice Arthur <arthurenochjustice@gmail.com>
2022-11-06 09:56:48 -0800

    commit task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,9 +10,9 @@ void puts2(char *str)
 {
 	int i, x;
 
-	for  (int i = 0; stri[i];)
+	for  (i = 0; str[i];)
 		i++;
 	for (x = 0; x >= i; x += 2)
-		_putchar(stri[x]);
+		_putchar(str[x]);
 	_putchar('\n');
 }
