9f2c6c4291cf910ba73dc053af3645f1f4b4e3d5
Enoch Justice Arthur <arthurenochjustice@gmail.com>
2022-11-06 05:09:24 -0800

    Task 0.98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * Description - Takes pointer parameter
+ * updates its coreesponding value to 98
+ * reset_to_98 - resets n to 98
+ * @n: interger to be changed
+ * Return -void
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
